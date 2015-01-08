62b74ba7245098b9d6bef7f4a3a3227c49edd34c
sonudhakar <sonu.dhakar@a-cti.com>
2015-01-08 19:46:36 +0530

    this is my second change

--- sample3/src/com/sample3/Sample3Servlet.java
+++ sample3/src/com/sample3/Sample3Servlet.java
@@ -10,5 +10,6 @@ public class Sample3Servlet extends HttpServlet {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
 		System.out.println("hello ji");
+		System.out.println("sonu ji");
 	}
 }
