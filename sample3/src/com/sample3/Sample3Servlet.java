f2af09f6632a2967d37ed4f0d43a2e3fb83d4ae8
sonudhakar <sonu.dhakar@a-cti.com>
2015-01-08 19:23:37 +0530

    this is new test

---
+++ sample3/src/com/sample3/Sample3Servlet.java
@@ -0,0 +1,13 @@
+package com.sample3;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Sample3Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
